e6df5778dab8c301fc42755797504b0198a3e3b1
Bruhbytes <dtsuthar6@gmail.com>
2023-06-16 19:24:08 +0530

    Revert "Modified the app.js file to output the current Day"
    This reverts commit f5a53cda992e35459ff1c56699cc924f48c7062d.
    
    I am reverting this commit because i forgot to include some additional
    Untracked files to this commit.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,20 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
 
 app.listen(3000, function(req, res){
     console.log("Server running on port 3000");
 })
 
 app.get('/', function(req, res){
-    
-    var today = new Date();
-    var currentDay = today.getDay();
-    
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var day = days[currentDay];
-
-    res.render('list', {kindOfDay : day});
+    res.send("<h2>Hello World</h2>");
 })
 
