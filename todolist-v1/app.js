a0cfaa4d9558c65495a9404c20f8ebd78ab202c5
Bruhbytes <dtsuthar6@gmail.com>
2023-06-18 16:14:57 +0530

    Basic Functionality of ToDoList completed
    The logic and html part of the web app is done.You can add items to
    your existing work list. Only the CSS styling is left.

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.listen(3000, function (req, res) {
     console.log("Server running on port 3000");
@@ -12,11 +13,23 @@ app.listen(3000, function (req, res) {
 app.get('/', function (req, res) {
     var today = new Date();
     var currentDay = today.getDay();
+    // var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    // var day = days[currentDay];
     
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var day = days[currentDay];
-    
-    res.render('list', { kindOfDay: day });
-    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    var day = today.toLocaleDateString('en-US', options);
+    res.render('list', { 
+        kindOfDay: day, 
+        list_items: items 
+    });
 })
 
+app.post('/', function(req, res){
+    item = req.body.next_item;
+    items.push(item);
+    res.redirect('/');
+})
