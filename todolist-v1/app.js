00e32fbb689fff2081da3381ccad0cdc8abd8f11
Bruhbytes <dtsuthar6@gmail.com>
2023-07-18 12:33:03 +0530

    Integrated MongoDB (mongoose) with our todolist project
    The project works fine, we can insert and delete items from our list
    and as a consequence it also affects our database hence it is responsive

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
-var items = [];
-var item = '';
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+
+const itemSchema = new mongoose.Schema({
+    name: String
+})
+
+const Item = new mongoose.model("item", itemSchema);
+
+const todoItem1 = new Item({
+    name: "Do Homework"
+})
+
+const todoItem2 = new Item({
+    name: "DSA"
+})
+
+var defaultItems = [todoItem1, todoItem2];
+const options = {ordered: true};
+
+
+var workItem = '';
 var workItems = [];
 
 app.listen(3000, function (req, res) {
@@ -17,22 +37,52 @@ app.listen(3000, function (req, res) {
 
 app.get('/', function (req, res) {
     var day = date.getDate();
-    res.render('list', { 
-        listType: day, 
-        list_items: items 
-    });
+
+    Item.find()
+    .then((items)=>{
+        if(items.length == 0){
+            Item.insertMany(defaultItems, options);
+            res.redirect('/');
+        }
+        else{
+            res.render('list', { 
+                listType: day, 
+                list_items: items 
+            });
+        }
+    })
+    .catch(err => console.log(err)) 
+
+    
 })
 
 app.post('/', function(req, res){
-    item = req.body.next_item;
+    const itemName = req.body.next_item;
+
+    const todoItem = new Item({
+        name: itemName
+    })
+    todoItem.save();
+    console.log("Successfully inserted");
+
     if(req.body.list == 'Work List'){
-        workItems.push(item);
+        workItems.push(workItem);
         res.redirect('/work');
     }
-    else{
-        items.push(item);
-        res.redirect('/');
-    }
+    
+    res.redirect('/');
+})
+
+app.post("/delete", function(req, res){
+    const del = req.body.deletedItem;
+    
+    Item.deleteOne({_id: del})
+    .then(() => {
+        console.log("Successfully deleted")
+    })
+    .catch(err => console.log(err));
+
+    res.redirect("/");
 })
 
 app.get('/work', function(req, res){
