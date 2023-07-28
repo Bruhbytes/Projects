b2f3f2ea7aefc353db1cab7b2403bfb353a82407
Bruhbytes <dtsuthar6@gmail.com>
2023-07-28 16:22:35 +0530

    BlogWebsite integrated with MongoDB
    Added gitignore file to ignore the node modules and env file to hide
    mongodb url.

--- BlogWebsite/app.js
+++ BlogWebsite/app.js
@@ -2,33 +2,81 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+const port = process.env.PORT || 3000;
 
 const homestarting = "Lorem ipsum dolor sit amet. Sed laborum dolorem qui aliquam voluptatem et accusantium aperiam ea incidunt provident et totam iusto sit repellat internos id reiciendis placeat. At atque dolor in autem voluptatum sed adipisci veniam et blanditiis quas et omnis impedit quo consectetur dolor. Et accusantium mollitia sed provident animi in corporis ipsam quo soluta veniam quo recusandae consequuntur et perferendis aperiam ab aliquid consequatur.";
 const aboutstarting = "33 rerum aliquam a quia facere vel sint libero et pariatur nulla et quia inventore et distinctio mollitia in atque animi. Non dolore reiciendis ut autem architecto est expedita totam rem autem eaque non officiis facere sed consequatur repellat. 33 repudiandae harum nam ducimus obcaecati non quia laboriosam? Non quod amet aut alias nihil et voluptatum aperiam qui dolores distinctio.";
 const contactstarting = "Et nostrum dicta eos laborum consequatur 33 molestias voluptatum est temporibus internos est iure ipsam et odio itaque. Hic ducimus omnis et porro labore aut magni aspernatur quo consequuntur quae. Non explicabo voluptatem qui magnam alias et fugit dolor aut recusandae esse aut necessitatibus omnis ad iste adipisci est deleniti veniam. Quo magni iste et repellat eligendi eum assumenda quod aut laborum galisum ea ullam dolorem ut dolorem reiciendis.";
-const posts = [];
 
-app.get('/', function(req, res){
-    res.render('home',{
-        "starting" : homestarting,
-        "blogs" : posts
-    });
+const url = process.env.MONGODB_URL;
+try {
+    mongoose.connect(url);
+    console.log("Successfully connected");
+} catch (error) {
+    console.log(error);
+}
+
+const blogSchema = new mongoose.Schema({
+    "title" : String,
+    "body" : String
 })
 
-app.get('/posts/:postTitle', function(req, res){
-    posts.forEach((post)=>{
-        if (_.lowerCase(req.params.postTitle) == _.lowerCase(post.title)){
-            res.render('post',{
-                "blogTitle" : post.title,
-                "blogContent" : post.body
-            })
-        }        
-    })           
+const Blog = mongoose.model('blog', blogSchema);
+
+const defaultblog = new Blog({
+    title: "Test 1",
+    body: "psum dolor sit amet. Sed laborum dolorem qui cdcsdvvrevd savrsbv tesb"
+})
+
+// defaultblog.save();
+
+app.get('/', function(req, res){
+    Blog.find()
+    .then((blogs) => {
+        res.render('home',{
+            "starting" : homestarting,
+            "blogs" : blogs
+        });
+    })
+    .catch(err => console.log(err));    
+})
+
+app.get('/posts/:postTitle', async function(req, res){
+    const btitle = req.params.postTitle;
+    
+    await Blog.findOne({title: btitle})
+    .then((blog) => {
+        res.render('post', {
+            blogTitle : blog.title,
+            blogContent : blog.body
+        })
+    })
+    .catch((err) => {
+        res.status(500).send("Internal Server Error!")
+        console.log(err);
+    })       
+})
+
+app.post('/delete', async function(req, res){
+    
+    const btitle = req.body.title;
+    await Blog.deleteOne({title: btitle})
+    .then(() => {
+        console.log("Blog deleted");
+    })
+    .catch((err) => {
+        res.status(500).send("Internal Server error");
+        console.log(err);
+    })
+    
+    res.redirect('/');
 })
 
 app.get('/about', function(req, res){
@@ -47,15 +95,16 @@ app.get('/compose', function(req, res){
     res.render('compose');
 })
 
-app.post('/compose', function(req, res){
-    const post = {
+app.post('/compose', async function(req, res){    
+    const blog = new Blog({
         title: req.body.postTitle,
         body: req.body.postContent
-    };
-    posts.push(post);
+    })
+    await blog.save();
+
     res.redirect('/');
 })
 
-app.listen(process.env.PORT || 3000, function(req, res){
-    console.log("Server up and running on port 3000");
+app.listen(port, function(req, res){
+    console.log(`Server up and running on port ${port}`);
 })
